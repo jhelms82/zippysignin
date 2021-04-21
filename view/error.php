8904c509c6e4f40d3bf7c076d091e5c1466aeb2b
James Helms <jhelms1014@gmail.com>
2021-04-21 12:09:50 -0500

    d

--- view/error.php
+++ view/error.php
@@ -1,6 +1,6 @@
-<?php //
-//include('../model/database.php');
-//include('header.php') ?>
-<!--<h2 class="top">Error</h2>-->
-<!--<p>--><?//= $error; ?><!--</p>-->
-<?php //include('footer.php') ?>
+<?php 
+include('../model/database.php');
+include('header.php') ?>
+<h2 class="top">Error</h2>
+<p><?= $error; ?></p>
+<?php include('footer.php') ?>
